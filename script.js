2ef1916dfda2cde95dc5b4ef2e4ac7556bb59262
victorfaller17 <vfvictor31@gmail.com>
2025-02-26 21:06:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,9 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
 });
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+botaoDeAcessibilidade.addEventListener('click', function (){
+ botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+ opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+})
