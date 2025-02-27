f91e0d2dc77466e61954d8fe7837464e0c8462af
victorfaller17 <vfvictor31@gmail.com>
2025-02-26 21:16:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,7 @@ botaoDeAcessibilidade.addEventListener('click', function (){
  botaoDeAcessibilidade.classList.toggle('rotacao-botao');
  opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 })
+const alternaContraste = document.getElementById('alterna-contraste')
+alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+ })
